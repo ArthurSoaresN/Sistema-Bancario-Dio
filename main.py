2a0037b29ad7cc7e30bba17b4858bcdc5ee57b18
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-03 23:53:11 -0300

    Cabeçario
    Explicando o intuito do desafio

---
+++ main.py
@@ -0,0 +1,7 @@
+#   Santander 2025 - Back-End com Python
+#   Desafio Criando um Sistema Bancário em Python
+#   Nivel Básico
+
+
+#   Sistema Bancário
+# Operações: Sacar, Depositar e Visualizar Extrato
