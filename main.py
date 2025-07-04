18242eba9a4ae0a0288f05e5b64bcf3cda184870
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-03 23:57:49 -0300

    Operações do sistema
    Requisitos do desafio para cada operação

--- main.py
+++ main.py
@@ -5,3 +5,18 @@
 
 #   Sistema Bancário
 # Operações: Sacar, Depositar e Visualizar Extrato
+
+#Saque 
+# O sistema deve permitir realiar 3 saques diários
+# Limite de R$500,00 por saque
+# Ter robustez
+# Armazenar todos os saques para operação Extrato
+
+#Depósito
+# Depositar valores positivos
+# Armazenar todos os saques para operação de Extrato
+
+#Extrato
+# Listar todos os depósitos e saques realizados na conta.
+# No fim da listagem deve ser exibido o saldo atual da conta.
+# Formato dos valores: R$ 100.00
