0d66d164a4474a32618f0022cf9ad73aa9ee2f64
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-24 23:41:01 -0300

    Testando pip: Flake8, Black e iSort

--- main.py
+++ main.py
@@ -6,29 +6,30 @@
 #   Sistema Bancário
 # Operações: Sacar, Depositar e Visualizar Extrato
 
-#Saque 
+# Saque
 # O sistema deve permitir realiar 3 saques diários
 # Limite de R$500,00 por saque
 # Ter robustez
 # Armazenar todos os saques para operação Extrato
 
-#Depósito
+# Depósito
 # Depositar valores positivos
 # Armazenar todos os saques para operação de Extrato
 
-#Extrato
+# Extrato
 # Listar todos os depósitos e saques realizados na conta.
 # No fim da listagem deve ser exibido o saldo atual da conta.
 # Formato dos valores: R$ 100.00
 
-import os
 import datetime
+import os
 
 LIMITE_DIARIO = 3
 SAQUE_LIMITE = 500
 AGENCIA = "0001"
 TRANSACOES_DIARIO = 10
 DATA_ATUAL = datetime.datetime.now()
+
 
 def set_menu(saques):
     menu = f"""    ===  Sistema Bancário DIO  ===
@@ -47,15 +48,21 @@ def set_menu(saques):
     7. Sair"""
     print(menu)
 
-def limpar_tela():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
 
-def depositar(valor_depositado: float, conta: float, extrato: list, transacoes_realizas: int) -> tuple[float, int]:
+def limpar_tela():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def depositar(
+    valor_depositado: float, conta: float, extrato: list, transacoes_realizas: int
+) -> tuple[float, int]:
     if transacoes_realizas == TRANSACOES_DIARIO:
-        print(f"Você atingiu o número máximo de transações diáras: {TRANSACOES_DIARIO} movimentações em {DATA_ATUAL.strftime('%d/%m/%Y às %H:%M')}")
+        print(
+            f"Você atingiu o número máximo de transações diáras: {TRANSACOES_DIARIO} movimentações em {DATA_ATUAL.strftime('%d/%m/%Y às %H:%M')}"
+        )
         return conta, transacoes_realizas
     elif valor_depositado <= 0:
         print("Não é permitido depositar saldo negativo")
@@ -66,17 +73,26 @@ def depositar(valor_depositado: float, conta: float, extrato: list, transacoes_r
         print(f"Deposito de R${valor_depositado:.2f} realizado com sucesso")
         print(f"Saldo Atual: {deposito:.2f}")
         transacoes_realizas = transacoes_realizas + 1
-        if extrato[0] == 'zero':
+        if extrato[0] == "zero":
             extrato[0] = f"Deposito de R${valor_depositado:.2f} realizado"
         else:
             extrato.append(f"Deposito de R${valor_depositado:.2f} realizado")
-        return deposito, transacoes_realizas #Deposito Realizado
+        return deposito, transacoes_realizas  # Deposito Realizado
 
-def sacar(valor_solicitado: float, conta: float, saques_realizados: int, extrato: list, transacoes_realizas: int) -> tuple[float, int, int]:
+
+def sacar(
+    valor_solicitado: float,
+    conta: float,
+    saques_realizados: int,
+    extrato: list,
+    transacoes_realizas: int,
+) -> tuple[float, int, int]:
     saque = conta - valor_solicitado
 
     if transacoes_realizas == TRANSACOES_DIARIO:
-        print(f"Você atingiu o número máximo de transações diárias {TRANSACOES_DIARIO} movimentações em {DATA_ATUAL.strftime('%d/%m/%Y às %H:%M')}")
+        print(
+            f"Você atingiu o número máximo de transações diárias {TRANSACOES_DIARIO} movimentações em {DATA_ATUAL.strftime('%d/%m/%Y às %H:%M')}"
+        )
     elif saques_realizados >= LIMITE_DIARIO:
         print("Você atingiu o número máximo de saques hoje")
     elif valor_solicitado <= 0:
@@ -93,7 +109,7 @@ def sacar(valor_solicitado: float, conta: float, saques_realizados: int, extrato
         saques_realizados += 1
         transacoes_realizas += 1
 
-        if extrato[0] == 'zero':
+        if extrato[0] == "zero":
             extrato[0] = f"Saque de R${valor_solicitado:.2f} realizado"
         else:
             extrato.append(f"Saque de R${valor_solicitado:.2f} realizado")
@@ -101,12 +117,13 @@ def sacar(valor_solicitado: float, conta: float, saques_realizados: int, extrato
 
     return conta, saques_realizados, transacoes_realizas
 
-def exibir_extrato (conta: float, extrato: list):
+
+def exibir_extrato(conta: float, extrato: list):
     limpar_tela()
     print("=== EXTRATO ===")
     print()
 
-    if extrato[0] == 'zero':
+    if extrato[0] == "zero":
         print("Nenhuma operação realiada")
     else:
         for operacao in extrato:
@@ -116,17 +133,18 @@ def exibir_extrato (conta: float, extrato: list):
     print(f"Saldo Atual: R${conta:.2f}")
     print()
 
-def menu_opcao(): 
+
+def menu_opcao():
     print("\nOpções:")
     print(" 1. Voltar ao menu")
     print(" 2. Sair")
-    
-    while True: # Loop para garantir que a entrada seja válida
+
+    while True:  # Loop para garantir que a entrada seja válida
         try:
-            escolha2 = int(input('    >>> '))
+            escolha2 = int(input("    >>> "))
             if escolha2 == 1:
                 limpar_tela()
-                return 
+                return
             elif escolha2 == 2:
                 limpar_tela()
                 print("Obrigado por utilizar o Sistema Bancário DIO. Até mais!")
@@ -135,6 +153,7 @@ def menu_opcao():
                 print("Opção inválida. Digite 1 para Voltar ou 2 para Sair.")
         except ValueError:
             print("Entrada inválida. Por favor, digite um número.")
+
 
 # DESAFIO 2 -> Funções: Criar Usuário e Criar Conta Corrente
 
@@ -146,9 +165,10 @@ def menu_opcao():
 
 # Criar conta corrente:
 # O programa deve armazenar contas em uma lista, uma conta é composta por:
-# agencia, numero da conta e usuario. 
+# agencia, numero da conta e usuario.
 # o Numero da agencia é fixo: 0001
 # O usuario pode ter mais de uma conta, mas uma conta pertence a somente um usuario
+
 
 def criar_usuario(usuarios):
     cpf = input("Informe o CPF (Somente números)    >>> ")
@@ -157,37 +177,49 @@ def criar_usuario(usuarios):
     if usuario:
         print("Já existe usuário com esse CPF")
         return
-    
+
     nome = input("Informe seu nome completo:    >>> ")
     data_nascimento = input("Informe sua data de nascimento (dd-mm-aaaa):   >>> ")
-    endereco = input("Informe o endereço (logradouro, numero - bairro - cidade/sigla do Estado):    >>> ")
-    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereço": endereco})
+    endereco = input(
+        "Informe o endereço (logradouro, numero - bairro - cidade/sigla do Estado):    >>> "
+    )
+    usuarios.append(
+        {
+            "nome": nome,
+            "data_nascimento": data_nascimento,
+            "cpf": cpf,
+            "endereço": endereco,
+        }
+    )
     limpar_tela()
     print()
     print("Usuário criado com sucesso!")
     print(f"Olá!, {nome}")
     print()
 
+
 def filtrar_usuario(cpf, usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
     return usuarios_filtrados[0] if usuarios_filtrados else None
 
+
 def criar_conta(agencia, numero_conta, usuarios):
     cpf = input("Informe o CPF do usuario:  >>> ")
-    usuario = filtrar_usuario(cpf, usuarios) 
+    usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
         limpar_tela()
         print("Conta criada com sucesso!")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
-    
+
     print("Usuário não encontrado, fluxo de criação de conta encerrado!")
+
 
 def listar_contas(contas):
 
     if len(contas) == 0:
         print("Não há contas cadastradas.")
-    else:   
+    else:
         for conta in contas:
             linha = f"""
         Agencia: {conta['agencia']}
@@ -196,9 +228,10 @@ def listar_contas(contas):
         """
         print(linha)
 
-valor = 0.0 # Variavel que recebe o valor a ser retirado ou adicionado
-conta = 0.0 # Variavel que controla o saldo do usuário
-extrato = ['zero'] #Variavel para registrar as operações realizadas
+
+valor = 0.0  # Variavel que recebe o valor a ser retirado ou adicionado
+conta = 0.0  # Variavel que controla o saldo do usuário
+extrato = ["zero"]  # Variavel para registrar as operações realizadas
 saques_realizados = 0
 transacoes_realizas = 0
 usuarios = []
@@ -210,48 +243,54 @@ while True:
     set_menu(saques_realizados)
 
     try:
-        escolha = int(input('    >>> '))
+        escolha = int(input("    >>> "))
     except ValueError:
         limpar_tela()
         print("Entrada inválida. Por favor, digite um número de 1 a 4.")
-        menu_opcao() # Dá a opção de voltar ou sair após erro
-        continue # Volta para o início do loop
+        menu_opcao()  # Dá a opção de voltar ou sair após erro
+        continue  # Volta para o início do loop
 
-    limpar_tela() # Limpa a tela após a escolha do menu
+    limpar_tela()  # Limpa a tela após a escolha do menu
 
-    if escolha == 1: # Depósito
+    if escolha == 1:  # Depósito
         print("Digite o valor que deseja depositar:")
         try:
-            valor_digitado = float(input('    >>> '))
-            conta, transacoes_realizas = depositar(valor_digitado, conta, extrato, transacoes_realizas) # Atualiza transacoes_realizas
+            valor_digitado = float(input("    >>> "))
+            conta, transacoes_realizas = depositar(
+                valor_digitado, conta, extrato, transacoes_realizas
+            )  # Atualiza transacoes_realizas
         except ValueError:
             print("Valor inválido. Por favor, digite um número.")
         menu_opcao()
 
-    elif escolha == 2: # Saque
+    elif escolha == 2:  # Saque
         print("Digite o valor que deseja sacar:")
         print(f"Saldo Atual: {conta}")
         try:
-            valor_digitado = float(input('    >>> '))
+            valor_digitado = float(input("    >>> "))
             # A função sacar agora retorna uma tupla
-            conta, saques_realizados = sacar(valor_digitado, conta , saques_realizados, extrato, transacoes_realizas) # DESAFIO 2 -> KEYWORD ONLY
+            conta, saques_realizados = sacar(
+                valor_digitado, conta, saques_realizados, extrato, transacoes_realizas
+            )  # DESAFIO 2 -> KEYWORD ONLY
         except ValueError:
             print("Valor inválido. Por favor, digite um número.")
         menu_opcao()
 
-    elif escolha == 3: # Extrato
-        exibir_extrato(conta, extrato) # Passa o saldo e o extrato para a função / DESAFIO 2 -> HIBRIDO
+    elif escolha == 3:  # Extrato
+        exibir_extrato(
+            conta, extrato
+        )  # Passa o saldo e o extrato para a função / DESAFIO 2 -> HIBRIDO
         menu_opcao()
 
-    elif escolha == 4: # Novo Usuário
+    elif escolha == 4:  # Novo Usuário
         criar_usuario(usuarios)
         menu_opcao()
 
-    elif escolha == 5: # Listar Contas
+    elif escolha == 5:  # Listar Contas
         listar_contas(contas)
         menu_opcao()
-    
-    elif escolha == 6: # Nova Conta
+
+    elif escolha == 6:  # Nova Conta
         numero_conta = len(contas) + 1
         conta = criar_conta(AGENCIA, numero_conta, usuarios)
 
@@ -260,10 +299,10 @@ while True:
 
         menu_opcao()
 
-    elif escolha == 7: # Sair
+    elif escolha == 7:  # Sair
         print("Obrigado por utilizar o Sistema Bancário DIO. Até mais!")
         exit()
 
-    else: # Escolha inválida
+    else:  # Escolha inválida
         print("Opção inválida. Por favor, selecione uma opção de 1 a 7.")
         menu_opcao()
